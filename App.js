87c39757a9f860795d736fdf333bd8356b634b2f
elmergustavo <elmergustavo79@gmail.com>
2022-07-07 12:26:50 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,52 @@
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text className="text-red-700 py-6">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          <UserIcon size={35} color="#00CCBB" />
+          
+          
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
