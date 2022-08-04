cecfa654aad2038b0c9c63f81d49ef8b0cd87bc1
elmergustavo <elmergustavo79@gmail.com>
2022-08-04 03:37:37 -0600

    update menu

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -32,5 +32,5 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions
 export const selectBasketItems = (state) => state.basket.items;
 
 export const selecBasketItemsWithId = (state,id) => state.basket.items.filter((item) => item.id === id);
-
+export const selectBasketTotal = (state) => state.basket.items.reduce((total,item) => total += item.price, 0)
 export default basketSlice.reducer
